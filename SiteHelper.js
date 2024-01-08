0aabe6bc2406cea5b568a1c73e48aded10ce6e1e
Copy <71971770+Copy05@users.noreply.github.com>
2024-01-08 13:54:19 +0100

    CORE

---
+++ SiteHelper.js
@@ -0,0 +1,85 @@
+/**
+ * (c) Copy05 2023 - 2024. All Rights Reserved.
+ * 
+ * Yes i've used this on all sites i've ever developed.
+ */
+
+const Footer = document.getElementsByTagName("footer")[0]
+const Header = document.getElementsByTagName("header")[0]
+const MobileNavBar = document.getElementById("mobilenavbar")
+const Hamburger = document.getElementById("mnb-hamburger")
+
+const HeaderHTML = `
+<a href="index.html" class="logohref"><img src="./img/Icon.png" alt="CME Logo" class="logo cme-icon"></a>
+        <ul class="nav_links">
+            <li class="dktl"><a href="about.html" class="header-link dktl">About</a></li>
+            <li class="dktl"><a href="#" class="header-link dktl">Industries</a></li>
+            <li class="dktl"><a href="#" class="header-link dktl">Company</a></li>
+            <li id="mnb-hamburger"><a href="#" id="mnb-hamburger-btn">≡</a></li>
+        </ul>
+`
+
+const MobileNavBarHTML = `
+<div class="mnb_links">
+    <a class="mnb-link" href="about.html">About</a>
+    <a class="mnb-link" href="#">Industries</a>
+    <a class="mnb-link" href="#">Company</a>
+</div>
+`
+
+const FooterHTML = `
+<img src="./img/Icon.png" alt="CME Logo" class="cme-icon footer-logo">
+
+        <div class="centre">
+            <div class="footer-sections">
+                <div class="footersection">
+                    <p class="fs-head">Legal</p>
+                    <ul class="feet-nav_links">
+                        <li><a href="#" class="feet-link">Imprint</a></li>
+                        <li><a href="privacy.html" class="feet-link">Privacy Policy</a></li>
+                    </ul>
+                </div>
+    
+                <div class="footersection">
+                    <p class="fs-head">Company</p>
+                    <ul class="feet-nav_links">
+                        <li><a href="about.html" class="feet-link">About</a></li>
+                    </ul>
+                </div>
+
+                <div class="footersection">
+                    <p class="fs-head">Industries</p>
+                    <ul class="feet-nav_links">
+                        <li><a href="#" class="feet-link">Entertainment</a></li>
+                        <li><a href="#" class="feet-link">Infomationtechnology (IT)</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <p class="copyright">&copy; ${new Date().getFullYear().toString()}, CopyMedia. All Rights Reserved.</p>
+`
+
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    Footer.innerHTML = FooterHTML;
+    Header.innerHTML = HeaderHTML;
+    MobileNavBar.innerHTML = MobileNavBarHTML;
+
+    const Hamburger = document.getElementById("mnb-hamburger-btn")
+
+    Hamburger.addEventListener("click", (e) => {
+
+        if(MobileNavBar.style.display === "none"){
+            MobileNavBar.style.display = "block"
+            Hamburger.innerText = "X"
+            return;
+        }
+        if(MobileNavBar.style.display === "block"){
+            MobileNavBar.style.display = "none"
+            Hamburger.innerText = "≡"
+            return;
+        }
+
+    })
+
+})
