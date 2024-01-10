d4571dad06b741efc0287a11c6239e520b29c1e3
Copy <71971770+Copy05@users.noreply.github.com>
2024-01-10 09:48:59 +0100

    I've just added a slider and a sitemap not much

---
+++ onLoad.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+  var copiedLogos = document.getElementById('slider-items').cloneNode(true)
+  document.getElementById('slider').appendChild(copiedLogos)  
+})
