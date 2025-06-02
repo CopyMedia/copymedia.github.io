1299d36dee88fb0d1835610e35d970c67c887fcf
Copy <void@void.net>
2025-06-02 12:33:14 +0200

    Closed

--- onLoad.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-  var copiedLogos = document.getElementById('slider-items')?.cloneNode(true)
-  document.getElementById('slider')?.appendChild(copiedLogos)  
-})
